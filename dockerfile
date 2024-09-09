2fe6d10b56137d2f2acbe55db6abc9b21a96f106
woravitkatasila <woravit5972@gmail.com>
2024-09-10 00:23:10 +0700

    add cloud build

--- dockerfile
+++ dockerfile
@@ -1,8 +1,11 @@
-FROM golang:1.12 as builder
+FROM golang:1.12 AS builder
 WORKDIR /go/src/helloworld
 COPY . .
 RUN CGO_ENABLED=0 GOOS=linux go build -v -o helloworld
 
 FROM alpine
 COPY --from=builder /go/src/helloworld/helloworld /helloworld
+
+EXPOSE 8080
+
 CMD ["/helloworld"]
--- main.go
+++ main.go
@@ -19,6 +19,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Print("Hello world sample started.")
 	http.HandleFunc("/", handler)
-	port := "8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
